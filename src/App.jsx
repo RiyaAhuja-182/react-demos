95c7c124ab3b6c4886c8424c69661556e4d5bed5
Riya Ahuja <riya.m.ahuja182@gmail.com>
2025-02-21 23:46:46 +0530

    Accordion Component Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import Form from "./components/Form";
+import Accordion from "./components/Accordion";
 import Counter from "./components/Counter"
+import HiddenSearchBar from "./components/HiddenSearchBar"
 import Todos from "./components/Todos"
 import ToggleBackgroundColor from "./components/ToggleBackgroundColor"
+import {accordionData} from "./utils/content";
 
 const App = () =>{
   
   return (
   //  <Counter/>
   // <Todos />  
-  
-  <ToggleBackgroundColor/>
+  // <ToggleBackgroundColor/>
+  // <HiddenSearchBar/>
+  <div>
+      <div className="accordion">
+        {accordionData.map(({ title, content }) => (
+          <Accordion title={title} content={content} />
+        ))}
+      </div>
+    </div>
+  // <Form />
   )
 }
 
